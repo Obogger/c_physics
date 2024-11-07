b9ff4b1f8e097dd09efede6329fa01be9a3788d4
Obogg <kevdogfara@outlook.com>
2024-11-07 10:17:16 +0100

    Added click to place balls

--- main.c
+++ main.c
@@ -20,10 +20,10 @@ struct ball_s
     SDL_Texture *texture;
 };
 
-#define WINDOWX 1920
-#define WINDOWY 1080
+#define WINDOWX 900
+#define WINDOWY 900
 #define GRAVITY 982
-#define MAX_BALLS 75
+#define MAX_BALLS 500
 
 
 SDL_Texture* load_texture(const char *file, SDL_Renderer *renderer); 
@@ -33,7 +33,6 @@ void collision_check(struct ball_s *balls, int ball_n);
 bool single_ball_check(struct ball_s *balls, int ball_n, struct ball_s ball);
 
 float get_distance(float x1, float y1, float x2, float y2);
-void clear_balls(struct ball_s *balls, int ball_n);
 
 
 struct ball_s create_ball(SDL_Renderer* renderer);
@@ -54,7 +53,7 @@ int main(void) {
                                           SDL_WINDOWPOS_CENTERED, 
                                           SDL_WINDOWPOS_CENTERED, 
                                           WINDOWX, WINDOWY, 
-                                          SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN);
+                                          SDL_WINDOW_SHOWN);
     if (!window) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -67,7 +66,6 @@ int main(void) {
     int quit = 0;
     SDL_Event event;
 
-    // Main event loop
     struct ball_s balls[MAX_BALLS];
     int ball_n = 1;
     for(int i = 0; i < ball_n; i++)
@@ -81,7 +79,7 @@ int main(void) {
     double delta_time = 0.0;
     double run_time = 0;
 
-    SDL_Texture *spriteTexture = loadTexture("circle.png", renderer);
+    SDL_Texture *spriteTexture = load_texture("circle.png", renderer);
     while (!quit) {
         Uint64 currentTime = SDL_GetPerformanceCounter();
         delta_time = (double)(currentTime - last_time) / SDL_GetPerformanceFrequency();
@@ -90,15 +88,27 @@ int main(void) {
         printf("FPS: %.0f MS: %.05f\t Ball: %d\tTotaltime: %.1f\n", 1 / delta_time, delta_time, ball_n, run_time);
         fflush(stdout);
 
-
         // Handle events
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 quit = 1;
             }
+            else if(event.type == SDL_MOUSEBUTTONDOWN && ball_n < MAX_BALLS)
+            {
+                struct ball_s ball = create_ball(renderer);
+                ball.x = event.button.x - ball.r;                
+                ball.y = event.button.y - ball.r;
+                if(!single_ball_check(balls, ball_n, ball))
+                {
+                    balls[ball_n] = ball;
+                    ball_n++;
+                    run_time = 0;
+                }
+
+            }
         }
-        // (Optional) Clear the window with a color
-        SDL_SetRenderDrawColor(SDL_GetRenderer(window), 17, 11, 36, 255); // Set black color
+
+        SDL_SetRenderDrawColor(SDL_GetRenderer(window), 17, 11, 36, 255);
         SDL_RenderClear(SDL_GetRenderer(window));
 
         apply_velocity_change(balls, ball_n, delta_time);
@@ -113,7 +123,7 @@ int main(void) {
             SDL_RenderCopy(renderer, balls[i].texture, NULL, &destRect);
         }
     
-        if(ball_n < MAX_BALLS && run_time > 0.5)
+        if(ball_n < MAX_BALLS && run_time > 9999)
         {
             struct ball_s ball = create_ball(renderer);
             if(!single_ball_check(balls, ball_n, ball))
@@ -138,7 +148,7 @@ int main(void) {
  
 
         SDL_RenderPresent(renderer); // Update the window
-        //SDL_Delay(16);
+        //SDL_Delay(64);
     }
 
     // Destroy the window and quit SDL
@@ -189,7 +199,7 @@ struct ball_s create_ball(SDL_Renderer* renderer)
     ball.color.r = rand() % 256;
     ball.color.g = rand() % 256;
     ball.color.b = rand() % 256;
-    ball.texture = loadTexture("circle.png", renderer);
+    ball.texture = load_texture("circle.png", renderer);
     SDL_SetTextureColorMod(ball.texture, ball.color.r, ball.color.g, ball.color.b);
 
     return ball;
@@ -235,7 +245,8 @@ void collision_check(struct ball_s *balls, int ball_n)
         }
         for(int j = i + 1; j < ball_n; j++)
         {
-            float distance = get_distance(balls[i].x + balls[i].r / 2, balls[i].y + balls[i].r / 2, balls[j].x + balls[j].r / 2, balls[j].y + balls[j].r / 2);
+            float distance = get_distance(balls[i].x + balls[i].r, balls[i].y + balls[i].r, balls[j].x + balls[j].r, balls[j].y + balls[j].r);
+            
             //If balls are closer than collective radius
             if(distance <= balls[i].r + balls[j].r)
             {
